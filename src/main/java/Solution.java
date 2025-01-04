1c95f4f72ed9f14871a9b1fee18e5e3b4083e64a
alek <wait82985@gmail.com>
2025-01-04 10:56:24 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,39 +1,49 @@
 import java.util.*;
 
-public class Solution {
+class Solution {
     public int countPalindromicSubsequence(String s) {
-        int result = 0;
-        Map<Character, int[]> map = new HashMap<>();
-        for (int i = 'a'; i <= 'z' ; i++) {
-            map.put((char)i, new int[]{-1, - 1});
-        }
+        int n = s.length();
+        int[][] firstLastIndexes = new int[26][2];
+        for(int i = 0; i < 26; i++)
+            Arrays.fill(firstLastIndexes[i], -1);
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if(map.get(ch)[0] == -1){
-                int[] indexes = map.get(ch);
-                indexes[0] = i;
-                map.put(ch, indexes);
-            }else if(map.get(ch)[0] != -1 && i - map.get(ch)[0] > 1 ){
-                int[] indexes = map.get(ch);
-                indexes[1] = i;
-                map.put(ch, indexes);
+        firstLastIndexes[s.charAt(0) - 'a'][0] = 0;
+        int[][] ps = new int[n][26];
+        int[] ps0 = new int[26];
+        ps0[s.charAt(0) - 'a']++;
+        ps[0] = ps0;
+        for(int i = 1; i < n; i++) {
+            if(firstLastIndexes[s.charAt(i) - 'a'][0] == -1) {
+                firstLastIndexes[s.charAt(i) - 'a'][0] = i;
+            }else {
+                firstLastIndexes[s.charAt(i) - 'a'][1] = i;
             }
+            ps[i] = Arrays.copyOf(ps[i - 1], 26);
+            ps[i][s.charAt(i) - 'a']++;
         }
-        for (Map.Entry<Character, int[]> e: map.entrySet()) {
-            char letter = e.getKey();
-            int[] indexes = e.getValue();
-            if(indexes[0] == -1 || indexes[1] == -1){
+        int res = 0;
+
+        for(int i = 0; i < 26; i++) {
+            if(firstLastIndexes[i][1] == -1 || firstLastIndexes[i][0] == -1){
                 continue;
             }
-            Set<Character> distinct = new HashSet<>();
-            for (int i = indexes[0] + 1; i < indexes[1]; i++) {
-                distinct.add(s.charAt(i));
+
+            int start  = firstLastIndexes[i][0];
+            int end = firstLastIndexes[i][1];
+            if(end - start == 1){
+                continue;
             }
-            int count = distinct.size();
-            result += count;
+
+            for(int j = 0; j < 26; j++) {
+                if(ps[end - 1][j] - ps[start][j] > 0) {
+                    res++;
+                }
+            }
+
+
         }
 
-        return result;
+
+        return res;
     }
 }
